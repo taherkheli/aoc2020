67f46fc421a62fd07cabfd80b35a3f9e8bb3bfc0
Bilal Taherkheli <bilal76@gmail.com>
2020-12-03 17:01:38 +0100

    D03 done

---
+++ src/D03/Point.cs
@@ -0,0 +1,16 @@
+﻿namespace aoc.D03
+{
+	public struct Point
+	{
+		public Point(int x, int y, bool isTree)
+		{
+      X = x;
+      Y = y;
+      T = isTree;
+    }
+
+    public int X { get; }
+    public int Y { get; }
+    public bool T { get; set; }
+  }
+}
---
+++ test/D03/TC01.cs
@@ -0,0 +1,77 @@
+﻿using aoc.D03;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace aoc.test.D03
+{
+	[TestClass]
+	public class TC01
+	{
+		[TestMethod]
+		public void InitialI()
+		{
+			var expected = 7;
+
+			var g = new Grid("./D03/example.txt");
+
+			var actual = g.TreesEncountered(3, 1);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void PartI()
+		{
+			var expected = 159;
+
+			var g = new Grid("./D03/input.txt");
+
+			var actual = g.TreesEncountered(3, 1);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void InitialII()
+		{
+			var expected = 336;
+
+			var g = new Grid("./D03/example.txt");
+
+			long product = 1;
+			product *= g.TreesEncountered(1, 1);
+			g.reset();
+			product *= g.TreesEncountered(3, 1);
+			g.reset();
+			product *= g.TreesEncountered(5, 1);
+			g.reset();
+			product *= g.TreesEncountered(7, 1);
+			g.reset();
+			product *= g.TreesEncountered(1, 2);
+			var actual = product;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void PartII()
+		{
+			var expected = 6419669520;
+
+			var g = new Grid("./D03/input.txt");
+
+			long product = 1;
+			product *= g.TreesEncountered(1, 1);
+			g.reset();
+			product *= g.TreesEncountered(3, 1);
+			g.reset();
+			product *= g.TreesEncountered(5, 1);
+			g.reset();
+			product *= g.TreesEncountered(7, 1);
+			g.reset();
+			product *= g.TreesEncountered(1, 2);
+			var actual = product;
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
