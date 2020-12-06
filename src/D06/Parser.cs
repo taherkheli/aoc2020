f5577c589d4af3b7870ec2a52b9a727c49be72a0
Bilal Taherkheli <bilal76@gmail.com>
2020-12-06 15:57:23 +0100

    Day06 done

---
+++ src/D06/Parser.cs
@@ -0,0 +1,21 @@
+﻿using aoc.Shared;
+using System.Collections.Generic;
+using System.IO;
+
+namespace aoc.D06
+{
+	public static class Parser
+	{	
+		public static List<Group> Parse(string path)
+		{
+			var lines = File.ReadAllLines(path);
+			var result = new List<Group>();
+			var chunks = Lib.GetEmptyLineSeparatedChunks(lines);
+
+			foreach (var chunk in chunks)
+				result.Add(new Group(chunk.ToArray()));
+
+			return result;
+		}
+	}
+}
---
+++ test/D06/TC01.cs
@@ -0,0 +1,53 @@
+﻿using aoc.D06;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace aoc.test.D06
+{
+	[TestClass]
+	public class TC01
+	{
+		List<Group> groupsExample = Parser.Parse("./D06/example.txt");
+		List<Group> groupsInput = Parser.Parse("./D06/input.txt");
+
+		[TestMethod]
+		public void InitialI()
+		{
+			var expected = 11;
+
+			var actual = Group.GetAnyoneAnsweredYesCount(groupsExample);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void PartI()
+		{
+			var expected = 6443;
+
+			var actual = Group.GetAnyoneAnsweredYesCount(groupsInput);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void InitialII()
+		{
+			var expected = 6;
+
+			var actual = Group.GetEveryoneAnsweredYesCount(groupsExample);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void PartII()
+		{
+			var expected = 3232;
+
+			var actual = Group.GetEveryoneAnsweredYesCount(groupsInput);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
