e9a4f919c00c1aac7bae9ccc42e273b654a6fa1b
Bilal Taherkheli <bilal76@gmail.com>
2020-12-08 11:15:37 +0100

    D7 done

---
+++ src/D07/Bag.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace aoc.D07
+{
+	public class Bag
+	{
+		private string _color;
+		private Dictionary<string, int> _table;
+
+		public Bag(string color)
+		{
+			_color = color;
+			_table = new Dictionary<string, int>();
+		}
+
+		public string Color { get { return _color; } }
+
+		public Dictionary<string, int> Table { get { return _table; } }
+
+
+		public bool CanHoldShinyGoldBag(List<Bag> list)
+		{
+			bool result = false;
+			int value = -1;
+
+			if (_table.Count != 0) //no bags
+			{
+				if (_table.TryGetValue("shiny gold", out value)) //child
+					result = true;
+				else //descendent
+				{
+					foreach (var entry in _table)
+					{
+						var tempBag = list.Find(b => b.Color == entry.Key);
+
+						if (tempBag.CanHoldShinyGoldBag(list))
+						{
+							result = true;
+							break;
+						}
+					}
+				}
+			}
+
+			return result;
+		}
+
+		public int TotalBagsNeeded(List<Bag> list)
+		{
+			int result = 1;  //itself
+			
+			if (_table.Count != 0) //descendents
+			{
+				foreach (var entry in _table)
+				{
+					var num = entry.Value;
+					var tempBag = list.Find(b => b.Color == entry.Key);
+					var insideIt = tempBag.TotalBagsNeeded(list);
+
+					result += (num * insideIt);
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ test/D07/TC01.cs
@@ -0,0 +1,82 @@
+﻿using aoc.D07;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace aoc.test.D07
+{
+	[TestClass]
+	public class TC01
+	{
+		List<Bag> bags = Parser.Parse("./D07/input.txt");
+		List<Bag> bags_ex1 = Parser.Parse("./D07/ex1.txt");
+		List<Bag> bags_ex2 = Parser.Parse("./D07/ex2.txt");
+
+		[TestMethod]
+		public void InitialI()
+		{
+			var expected = 4;
+			int count = 0;
+
+			foreach (var bag in bags_ex1)
+				if (bag.CanHoldShinyGoldBag(bags_ex1))
+					count++;
+
+			var actual = count;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void PartI()
+		{
+			var expected = 226;
+			int count = 0;
+
+			foreach (var bag in bags)
+				if (bag.CanHoldShinyGoldBag(bags))
+					count++;
+
+			var actual = count;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void InitialIIa()
+		{
+			var expected = 32;
+
+			var bag = bags_ex1.Find(b => b.Color == "shiny gold");
+
+			var actual = bag.TotalBagsNeeded(bags_ex1) - 1;  //excluding itself
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void InitialIIb()
+		{
+			var expected = 126;
+			List<Bag> bags_temp = Parser.Parse("./D07/ex2.txt");
+
+			var bag = bags_ex2.Find(b => b.Color == "shiny gold");
+
+			var actual = bag.TotalBagsNeeded(bags_ex2) - 1;  //excluding itself
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void PartII()
+		{
+			var expected = 9569;
+			List<Bag> bags_temp = Parser.Parse("./D07/ex2.txt");
+
+			var bag = bags.Find(b => b.Color == "shiny gold");
+
+			var actual = bag.TotalBagsNeeded(bags) - 1;
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
