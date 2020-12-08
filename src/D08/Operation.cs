3f23abb8f6c23d3fb798fb75e4a50c9a5a6f3031
Bilal Taherkheli <bilal76@gmail.com>
2020-12-09 00:27:25 +0100

    D08 finished

---
+++ src/D08/Operation.cs
@@ -0,0 +1,10 @@
+﻿namespace aoc.D08
+{
+	public enum Operation
+	{
+		Accumulate = 0,
+		Jump = 1,
+		NOP = 2,
+		Unknown
+	}
+}
---
+++ test/D08/TC01.cs
@@ -0,0 +1,37 @@
+﻿using aoc.D08;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace aoc.test.D08
+{
+	[TestClass]
+	public class TC01
+	{
+		[TestMethod]
+		[DataRow("./D08/ex.txt", 5)]
+		[DataRow("./D08/input.txt", 1420)]
+
+		public void PartI(string path, int expected)
+		{
+			var program = Parser.Parse(path);
+			var b = new BalilInside(program);
+
+			var actual = b.PartI();
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		[DataRow("./D08/ex.txt", 8)]
+		[DataRow("./D08/input.txt", 1245)]
+		public void PartII(string path, int expected)
+		{
+			var program = Parser.Parse(path);
+
+			var b = new BalilInside(program);
+
+			var actual = b.PartII();
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
