9cf27522717c331210253b6c1d84573563b48c85
Bilal Taherkheli <bilal76@gmail.com>
2021-01-03 18:31:59 +0100

    D14 partI done

---
+++ src/D14/Emulator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aoc.D14
+{
+  public class Emulator
+  {
+    private Dictionary<long, String> _memory;
+
+    public Emulator()
+    {
+      _memory = new Dictionary<long, string>();
+    }
+
+    public long PartI(List<Tuple<string, long, long>> list) 
+    {
+      string mask;
+      long address;
+      long value;
+      long sum = 0;
+
+      foreach (var item in list)
+      {
+        mask = item.Item1;
+        address = item.Item2;
+        value = item.Item3;
+
+        var values_s = ApplyMask(mask, value);
+        Store(address, values_s);
+      }
+
+      foreach (var item in _memory)
+        sum += Convert.ToInt64(item.Value, 2);
+
+      return sum;
+    }
+
+    private string ApplyMask(string mask, long num)
+    {
+      var sb = new StringBuilder();
+      var num_s = Convert.ToString(num, 2).PadLeft(36, '0');
+
+      int i = 0;
+
+      foreach (var c in mask)
+      {
+        if ((c == '1') || (c == '0'))
+          sb.Append(c);
+        else
+          sb.Append(num_s[i]);
+
+        i++;
+      }
+
+      return sb.ToString();
+    }
+
+    private void Store(long address, string value)
+    {
+      string v;
+
+      if (_memory.TryGetValue(address, out v))  //update exisiting mem location
+        _memory[address] = value;
+      else
+        _memory.Add(address, value);
+    }
+  }
+}
---
+++ test/D14/TC01.cs
@@ -0,0 +1,23 @@
+﻿using aoc.D14;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace aoc.test.D14
+{
+  [TestClass]
+  public class TC01
+  {
+    [TestMethod]
+    [DataRow("./D14/ex.txt", 165)]
+    [DataRow("./D14/input.txt", 17765746710228)]
+    public void PartI(string path, long expected)
+    {
+      var list = Parser.Parse(path);
+      var memory = new Emulator();
+
+      var actual = memory.PartI(list);
+
+      Assert.AreEqual(expected, actual);
+    }
+
+  }
+}
